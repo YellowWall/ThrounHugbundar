372d7c908d366836c118d46ef8f4a671417b74e7
mariaorra <mao44@hi.is>
2023-03-23 16:41:32 +0000

    Update Flight.java
    Setja upp Flight.java

---
+++ Flight.java
@@ -0,0 +1,82 @@
+import java.util.Date;
+
+public class Flight implements flightInterface {
+
+    @Override
+    public void setLocation(Location loc) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setLocation'");
+    }
+
+    @Override
+    public Location getLocation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLocation'");
+    }
+
+    @Override
+    public String[] getSeatsLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSeatsLeft'");
+    }
+
+    @Override
+    public void setSeatTaken(int x, char a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSeatTaken'");
+    }
+
+    @Override
+    public void setSeatAvail(int x, char a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSeatAvail'");
+    }
+
+    @Override
+    public void setCapacity(int x) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCapacity'");
+    }
+
+    @Override
+    public int getCapacity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCapacity'");
+    }
+
+    @Override
+    public void setDate(Date date) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDate'");
+    }
+
+    @Override
+    public Date getDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getDate'");
+    }
+
+    @Override
+    public void setFlightNo(String str) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFlightNo'");
+    }
+
+    @Override
+    public String getFlightNo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFlightNo'");
+    }
+
+    @Override
+    public void setPrice(Float fl) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPrice'");
+    }
+
+    @Override
+    public Float getPrice() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrice'");
+    }
+}
