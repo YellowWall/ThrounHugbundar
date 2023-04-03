e57faebabc0a12257675c35f084935ff1b2d9ff8
YellowWall <ivarsig94@gmail.com>
2023-04-03 23:51:58 +0000

    fyrsta sql skipun skrifuð

--- src/repositories/BookingRepository.java
+++ src/repositories/BookingRepository.java
@@ -5,47 +5,122 @@ import src.vinnsla.Booking;
 import src.vinnsla.Flight;
 import src.vinnsla.Seat;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BookingRepository {
+    private String connectionUrl = "jdbc:sqlserver://dpg-cghgmrg2qv23kcqt4800-a.frankfurt-postgres.render.com/hbv401g_database;"
+        + "database=hbv401g_database;"
+        + "user=hbv401g_database_user@yourserver;"
+        + "password=f6oiteRl2PFv8NuE0cCGZP8XpBbMg7SS;"
+        + "encrypt=true;"
+        + "trustServerCertificate=true;"
+        + "loginTimeout=30;";
 
     public BookingRepository(){
+    }  
 
-    }
 
-    public Seat getInFlightSeats(Flight flight){
+    public Seat[] getInFlightSeats(Flight flight){
+        ResultSet resultSet = null;
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
+            String selectSql = "Select Seats.seat "+ 
+            "from Seats Left Join "+ 
+            "tickets on Seats.ticket = ticket.id "+ 
+            "left join Bookings on "+ 
+            "ticket.booking = Bookings.id "+ 
+            "left join DateFlight on "+ 
+            "Bookings.flight = DateFlight.id "+ 
+            "left join Flight on "+
+            "DateFlight.flight = Flight.id "+
+            "where Flight.FlightNum = "+ flight.getFlightNo()+
+            " and DateFlight.Date = "+ flight.getDate();
+            Statement stmnt = connection.createStatement();
+            resultSet= stmnt.executeQuery(selectSql);
+            while(resultSet.next()){
+                System.out.println(resultSet.getString(0));
+            }return new Seat[0];
+        }catch(SQLException e){
+            e.printStackTrace();
+            return new Seat[0];
+        }
+        
 
     }
     public int getSpaceInFlight(Flight flight){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
     public List<Booking> getBookings() {
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
     public List<Booking> createBooking(Booking booking){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
     public boolean cancelBooking(Booking booking){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
 
     }
 
     public boolean reserveSeats(){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
     public boolean createTicket(){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
     public boolean removeTicket(){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public boolean removeSeat(){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public Booking updateBooking(Booking booking){
+        try(Connection connection = DriverManager.getConnection(connectionUrl);){
 
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
