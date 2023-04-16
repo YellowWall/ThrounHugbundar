2eab83b7392a708482258ebae99bb2a221ba256f
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2023-04-16 10:59:25 +0000

    græja seats í flight.java

--- src/vinnsla/Flight.java
+++ src/vinnsla/Flight.java
@@ -1,5 +1,7 @@
 package src.vinnsla;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Flight implements flightInterface {
     private String flightNo;
@@ -7,13 +9,27 @@ public class Flight implements flightInterface {
     private Location loc;
     private int capacity;
     private float price;
-    private String[] seats;
+    private List<String> seats;
+    private List<String> seatsLeft;
+    private char[] seatLetters= {'a', 'b', 'c', 'd', 'e', 'f'};
 
     public Flight(Date dat, Location loc,String flightNo, int cap) {
         date = dat;
         this.loc = loc;
         this.flightNo = flightNo;
         capacity = cap;
+        seats = new ArrayList<>(cap);
+        seatsLeft = new ArrayList<>(cap);
+
+        for (int i = 1; i <= cap; i++) {
+            for (int j = 0; j < 6; j++) {
+                char row = (char)(i + '0');
+                char seatLetter = seatLetters[j];
+                String seat = new String(new char[] {row, seatLetter});
+                seats.add(seat);
+                seatsLeft.add(seat);
+            }
+        }
     }
 
     @Override
@@ -35,8 +51,11 @@ public class Flight implements flightInterface {
         throw new UnsupportedOperationException("Unimplemented method 'getSeatsLeft'");
     }
 
+    /*
+     * seat must be a non-capital letter
+     */
     @Override
-    public void setSeatTaken(int x, char a) {
+    public void setSeatTaken(int row, char seat) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setSeatTaken'");
     }
