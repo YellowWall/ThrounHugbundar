c4bc0c5b53a42f1d50ef299da48336a5ddc1b988
Ingimargudna <92264956+Ingimargudna@users.noreply.github.com>
2023-04-16 11:23:45 +0000

    smá lag fyrir exception

--- src/vinnsla/flightInterface.java
+++ src/vinnsla/flightInterface.java
@@ -5,7 +5,7 @@ interface flightInterface{
 	public void setLocation(Location loc);
 	public Location getLocation();
 	public String[] getSeatsLeft();
-	public void setSeatTaken(int x,char a);
+	public void setSeatTaken(int x,char a) throws Exception;
 	public void setSeatAvail(int x,char a);
 	public void setCapacity(int x);
 	public int getCapacity();
